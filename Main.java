98d763f79e63c1a11f66dcf086580fd50797ad5b
luisplacco <lfelipe0507@gmail.com>
2025-01-23 17:14:21 -0300

    Created script to calculate IMC and  the compiler.bat file to compile the code with Java version 8.

---
+++ Main.java
@@ -0,0 +1,38 @@
+import java.util.Scanner; // Importa a classe Scanner para entrada de dados
+import java.util.Locale;
+public class Main { // Nome da classe deve ser o mesmo do arquivo (Main.java, por exemplo)
+    public static void main(String[] args) {
+
+      
+        Scanner scanner = new Scanner (System.in).useLocale(Locale.forLanguageTag("US")); 
+            System.out.println("qual seu peso?");
+            double peso = scanner.nextDouble();
+            System.out.println("qual sua altura?");
+            double altura = scanner.nextDouble();
+            double imc = peso/(altura*altura);
+            String formatarimc = String.format("%.2f", imc);
+
+            if (imc < 18.5){
+            
+                System.out.print("seu imc é:" + formatarimc + " vc esta abaixo do peso");
+            }
+            else if (imc > 18.5 && imc < 24.9) {
+
+                System.out.print("seu imc é:" + formatarimc + " vc esta no peso normal");
+            }
+            else if (imc > 24.9 && imc < 29.9){
+                System.out.print("seu imc é: " + formatarimc + " vc esta no sobrepeso :/");
+            }
+            else if(imc > 29.9 && imc <39.9){
+                System.out.print("seu imc é: "+ formatarimc + " vc esta obeso");
+            }
+            else {
+                System.out.print("seu imc é:" + formatarimc+ " vc esta obeso morbido");
+            }
+
+        
+        }
+            
+    }
+
+
---
+++ README.md
@@ -0,0 +1,9 @@
+# IMC Calculator in Java
+
+Este é um projeto simples em Java que calcula o Índice de Massa Corporal (IMC) de uma pessoa com base no peso e altura fornecidos.
+
+## Como usar
+
+1. Clone este repositório para sua máquina local:
+   ```bash
+   git clone https://github.com/luisplacco/imc-com-java.git
